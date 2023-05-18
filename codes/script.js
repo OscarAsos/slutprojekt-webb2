b13500a4ee004738fd41617fe184fb082cdf2913
OscarAsos <104981299+OscarAsos@users.noreply.github.com>
2023-05-18 21:19:00 +0200

    Index done, progress on 2nd page

--- codes/script.js
+++ codes/script.js
@@ -31,3 +31,34 @@ heartDOM.onclick = (event) => {
 heartDOM.addEventListener('animationend', (event) => {
 	event.currentTarget.classList.remove('pulse');
 })
+
+
+var canvasElement = document.getElementById("aktivitetChart");
+
+
+var config	= {
+	type: "bar",
+	data: {
+	labels: ["08:00 - 10:00", "10:00 - 12:00", "12:00 - 14:00", "14:00 - 16:00", "16:00 - 18:00"],
+	 datasets: [{label:"Antal kunder i salongen",
+	data:[2,4,5,3,2],
+	backgroundColor:[
+		"rgba(75, 192, 192, 0.2)",//green
+		"rgba(225, 159, 64 0.2)", //orange
+		"rgba(255, 99, 132, 0.2)",//red
+		"rgba(225, 159, 64 0.2)", //orange
+		"rgba(75, 192, 192, 0.2)",//green
+	],
+	borderColor:[
+		"rgba(75, 192, 192, 1)",//green
+		"rgba(225, 159, 64 1)", //orange
+		"rgba(255, 99, 132, 1)",//red
+		"rgba(225, 159, 64 1)", //orange
+		"rgba(75, 192, 192, 1)",//green
+	],
+	borderWidth:1,
+}],
+	},
+};
+
+var aktivitetChart = new Chart(canvasElement, config)
